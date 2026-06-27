905f5e1ed1f30777692c6c0387da2847c62d4967
dattu-dev <uydat5002@gmail.com>
2026-06-28 01:06:45 +0700

    Adjust Elder NPC position Y to -0.354

--- Assets/Scripts/Chapters/ChapterFlowController.cs
+++ Assets/Scripts/Chapters/ChapterFlowController.cs
@@ -222,7 +222,7 @@ public class ChapterFlowController : MonoBehaviour
         var model = go.transform.Find("NpcModel");
         if (model != null)
         {
-            model.localPosition = Vector3.zero;
+            model.localPosition = new Vector3(0f, -0.354f, 0f);
             model.localScale = new Vector3(2.5f, 2.5f, 2.5f);
         }
         RegisterWaypoint("ask_elder", go.transform.position);
